3629c17ca472100606c62438e4af0feb8ad55960
Jay Juang <jae@jaeeee.local>
2020-02-25 08:41:44 +0800

    *

---
+++ fallback.js
@@ -0,0 +1,18 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!*********************!*\
+  !*** ./fallback.js ***!
+  \*********************/
+/*! unknown exports (runtime-defined) */
+/*! exports [maybe provided (runtime-defined)] [no usage info] */
+/*! runtime requirements:  */
+eval("\n\n//# sourceURL=webpack:///./fallback.js?");
+/******/ })()
+;
